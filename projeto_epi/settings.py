1a1247c8299458f46cf4efbbdbdf024590cf9207
Leonardo dos santos Rudek <rudekrudek2024@GMAIL.COM>
2025-10-18 10:55:01 -0300

    adctestefuncionais

--- projeto_epi/settings.py
+++ projeto_epi/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'epi_db',     # nome do banco
         'USER': 'root',       # usuário root do MySQL
-        'PASSWORD': '5636',
+        'PASSWORD': '123456',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
